c8648291541e47ec9fa481a0a814411369b9e93e
Guangming Mao <maogm12@gmail.com>
2023-01-17 15:21:49 -0800

    Few updates

--- lua/config/bufferline.lua
+++ lua/config/bufferline.lua
@@ -1,46 +1,73 @@
-require("bufferline").setup {
+require('bufferline').setup {
   options = {
-    numbers = "buffer_id",
-    close_command = "bdelete! %d",
-    right_mouse_command = nil,
-    left_mouse_command = "buffer %d",
-    middle_mouse_command = nil,
+    mode = "buffers", -- show buffer
+    numbers = "none",
+    close_command = "bdelete! %d", -- delete the buffer
+    right_mouse_command = "bdelete! %d", -- right click to delete the buffer
+    left_mouse_command = "buffer %d", -- left click to choose the buffer
+    middle_mouse_command = nil, -- no middle mouse click
     indicator = {
-      icon = "▎", -- this should be omitted if indicator style is not 'icon'
-      style = "icon",
+      style = 'underline'
     },
-    buffer_close_icon = "",
-    modified_icon = "●",
-    close_icon = "",
+    buffer_close_icon = '',
+    modified_icon = '●',
+    close_icon = '',
     left_trunc_marker = "",
     right_trunc_marker = "",
-    max_name_length = 18,
-    max_prefix_length = 15,
-    tab_size = 10,
-    diagnostics = false,
-    custom_filter = function(bufnr)
-      -- if the result is false, this buffer will be shown, otherwise, this
-      -- buffer will be hidden.
-
-      -- filter out filetypes you don't want to see
-      local exclude_ft = { "qf", "fugitive", "git" }
-      local cur_ft = vim.bo[bufnr].filetype
-      local should_filter = vim.tbl_contains(exclude_ft, cur_ft)
-
-      if should_filter then
-        return false
+    --- name_formatter can be used to change the buffer's label in the bufferline.
+    name_formatter = function(buf) -- buf contains a "name", "path" and "bufnr"
+      -- remove extension from markdown files for example
+      if buf.name:match('%.md') then
+        return vim.fn.fnamemodify(buf.name, ':t:r')
       end
-
-      return true
     end,
-    show_buffer_icons = false,
+    max_name_length = 18,
+    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
+    tab_size = 18,
+    diagnostics = "nvim_lsp",
+    diagnostics_update_in_insert = false,
+    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+      local s = " "
+      for e, n in pairs(diagnostics_dict) do
+        local sym = e == "error" and " "
+        or (e == "warning" and " " or "")
+        s = s .. sym
+      end
+      return s
+    end,
+    -- NOTE: this will be called a lot so don't do any heavy processing here
+    custom_filter = function(buf_number, buf_numbers)
+      -- filter out filetypes you don't want to see
+      if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+        return true
+      end
+      -- filter out by buffer name
+      if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+        return true
+      end
+      -- filter out based on arbitrary rules
+      -- e.g. filter out vim wiki buffer from tabline in your work repo
+      if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+        return true
+      end
+      -- filter out by it's index number in list (don't show first buffer)
+      if buf_numbers[1] ~= buf_number then
+        return true
+      end
+    end,
+    offsets = { { filetype = "bufferlist", text = "Explorer", text_align = "center" } },
+    color_icons = true,
+    show_buffer_icons = true, -- disable filetype icons for buffers
     show_buffer_close_icons = true,
+    show_buffer_default_icon = true, -- default icon
     show_close_icon = true,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = "bar",
+    -- can also be a table containing 2 custom separators
+    -- [focused and unfocused]. eg: { '|', '|' }
+    separator_style = "thin",
     enforce_regular_tabs = false,
     always_show_bufferline = true,
-    sort_by = "id",
-  },
+    sort_by = 'id'
+  }
 }
