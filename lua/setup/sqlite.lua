c2f913ddf2e8f81795c39d6495ba7c49790ea73a
Guangming Mao <maogm12@gmail.com>
2023-01-31 01:49:09 -0800

    Boot up sqlite.lua setup time

--- lua/setup/sqlite.lua
+++ lua/setup/sqlite.lua
@@ -1,4 +1,14 @@
-if 1 == vim.fn.executable "brew" then
+local default_system_sqlite_clib_path = "/usr/lib/libsqlite3.dylib"
+local default_brew_sqlite_clib_path = "/opt/homebrew/opt/sqlite/lib/libsqlite3.dylib"
+
+if vim.fn.filereadable(default_system_sqlite_clib_path) then
+  -- default system sqlite3 lib
+  vim.g.sqlite_clib_path = default_system_sqlite_clib_path
+elseif vim.fn.filereadable(default_brew_sqlite_clib_path) then
+  -- default brew installed sqlite3 lib
+  vim.g.sqlite_clib_path = default_brew_sqlite_clib_path
+elseif 1 == vim.fn.executable "brew" then
+  -- query brew for the sqlite3 lib path
   local Job = require("plenary.job")
   local sqlite_root, ret = Job:new({
     command = 'brew',
